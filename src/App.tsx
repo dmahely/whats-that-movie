d0ed0d1158274e5abdf0ae2a723e9232dc88192c
Doaa Mahely <dmahely@hotmail.com>
2023-03-07 20:11:24 +0400

    Update description in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="h1">
+        What's that movie?
+      </h1>
+      <div className="description">
+        Have you ever blanked on the name of a movie while you were talking about it but could recite each of the cast members names? This website is built to help you with that.
+      </div>
+      <input name="input" />
     </div>
   );
 }
