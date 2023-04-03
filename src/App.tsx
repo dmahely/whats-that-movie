8f8949129f98e26d94e2f2a6a7b9b48822f343fe
Doaa Mahely <dmahely@hotmail.com>
2023-04-04 01:49:43 +0400

    Add chakra ui and update description component

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,22 @@ import { Description } from './components/Description';
 import { InputForm } from './components/InputForm';
 import { Suggestions } from './components/Suggestions';
 import { Selections } from './components/Selections';
+import { ChakraProvider, VStack } from '@chakra-ui/react'
 
 const App = () => {
   const [actors, setActors] = useState<Array<Actor>>([])
   const [selectedActors, setSelectedActors] = useState<Array<Actor>>([])
 
   return (
-    <div className="App">
-      <Description />
-      <Selections selectedActors={selectedActors} />
-      <InputForm setActors={setActors} />
-      <Suggestions actors={actors} setSelectedActors={setSelectedActors} selectedActors={selectedActors} />
-    </div>
+    <ChakraProvider>
+      <VStack display-name="app-vstack" className="App">
+        <Description />
+        {selectedActors.length > 1 && <button>Find out the movie name!</button>}
+        <Selections selectedActors={selectedActors} />
+        <InputForm setActors={setActors} />
+        <Suggestions actors={actors} setSelectedActors={setSelectedActors} selectedActors={selectedActors} />
+      </VStack>
+    </ChakraProvider>
   );
 }
 
--- src/components/Description/Description.tsx
+++ src/components/Description/Description.tsx
@@ -1,18 +1,19 @@
+import { Heading, VStack, Flex, Text, Image } from '@chakra-ui/react';
 import image from '../../assets/movie_night.svg'
-import './Description.css'
 
 const Description = () => {
     return (
-        <div className="Description">
-            <h1 className="h1">
-                What's that movie?
-            </h1>
-            <p className="description">
-                Have you ever blanked on the name of a movie while you were talking about it but could recite each of the cast members names? This website was built to help you with that.
-            </p>
-
-            <img alt="Movie night" className="hero-image" src={image} />
-        </div>
+        <VStack display-name="description-vstack" mt={3} mb={5}>
+            <Flex gap={4} flexDir="column" w="100%" alignItems="center">
+                <Heading className="h1">
+                    What's that movie?
+                </Heading>
+                <Text className="description" w="50vw">
+                    Have you ever blanked on the name of a movie while you were talking about it but could recite each of the cast members names? This website was built to help you with that.
+                </Text>
+                <Image alt="Movie night" htmlWidth="350px" className="hero-image" src={image} />
+            </Flex>
+        </VStack>
     );
 }
 
