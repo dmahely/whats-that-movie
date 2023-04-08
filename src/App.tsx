a5ec3586f3f5b29131837c719436b5ab734a6b8b
Doaa Mahely <dmahely@hotmail.com>
2023-04-09 03:10:13 +0400

    Update logo and description

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { Description } from './components/Description';
 import { InputForm } from './components/InputForm';
 import { Suggestions } from './components/Suggestions';
 import { Selections } from './components/Selections';
-import { ChakraProvider, VStack, Center, Flex } from '@chakra-ui/react'
+import { ChakraProvider, VStack, Center, Flex, Heading } from '@chakra-ui/react'
 import { Footer } from './components/Footer';
 import { ResultButton } from './components/ResultButton';
 import { Result } from './components/Result';
@@ -39,9 +39,10 @@ const App = () => {
     <ChakraProvider>
       <Flex display-name="app-container-flex" flexDir="column" w="100%">
         <VStack display-name="app-vstack" className="App" minH="100vh">
-          <Description />
+          <Heading as="h1" m="2">What's that movie?</Heading>
           {appState === AppState.INITIAL &&
             <VStack display-name="app-initial-screen-vstack">
+              <Description />
               <ResultButton selectedActors={selectedActors} setCommonMovies={setCommonMovies} setAppState={setAppState} />
               <Selections selectedActors={selectedActors} setSelectedActors={setSelectedActors} />
               <InputForm inputValue={inputValue} setInputValue={setInputValue} setActors={setActors} />
--- src/components/Description/Description.tsx
+++ src/components/Description/Description.tsx
@@ -1,17 +1,16 @@
-import { Heading, VStack, Flex, Text, Image } from '@chakra-ui/react';
+import { Heading, VStack, Flex, Text, Image, Highlight } from '@chakra-ui/react';
 import image from '../../assets/movie_night.svg'
 
 const Description = () => {
     return (
-        <VStack display-name="description-vstack" mt={3} mb={5}>
+        <VStack display-name="description-vstack" mb={5}>
             <Flex gap={4} flexDir="column" w="100%" alignItems="center">
-                <Heading className="h1">
-                    What's that movie?
-                </Heading>
+                <Image alt="Movie night" boxSize="250px" className="hero-image" src={image} />
                 <Text className="description" w="50vw">
-                    Have you ever blanked on the name of a movie while you were talking about it but could recite each of the cast members names? This website was built to help you with that.
+                    <Highlight query="all the actors" styles={{ px: '1', py: '1', rounded: 'full', bg: 'purple.100', fontWeight: 'bold' }}>
+                        Have you ever blanked on the name of a movie while you were talking about it but could recite all the actors in it? This website was built to help you with that.
+                    </Highlight>
                 </Text>
-                <Image alt="Movie night" htmlWidth="350px" className="hero-image" src={image} />
             </Flex>
         </VStack>
     );
